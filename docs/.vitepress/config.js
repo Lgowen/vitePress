4785f792b582e6b0adab38ee21411e540138041c
Lgowen <434666361@qq.com>
2022-01-25 02:04:50 +0800

    feat: 增加环境变量判断设置不同的base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,10 +1,11 @@
 import { getLeetCodeMenuBar } from './menu'
-console.log(process.env)
+const env = process.env.NODE_ENV === 'development' ? '/' : '/vitePress/'
+
 module.exports = {
-  title: "测试一下自动化部署有没有成功", // 网站标题
+  title: "自动化部署成功啦！！！！！", // 网站标题
   description: "leetcode share.", //网站描述
 //   base: "/", //  部署时的路径 默认 /  可以使用二级地址 /base/
-  base: "/vitePress/",
+  base: env,
   // lang: 'en-US', //语言
   // 网页头部配置，引入需要图标，css，js
   //   head: [
